33978a6c490799c383be4790e59c680d116f457f
yvaine <jingleng77@gmail.com>
2015-12-14 21:54:16 +0800

    test cmake config

---
+++ part_1/src/hello.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: hello.cpp
+    > Author: lengjing
+    > Mail: jingleng77@sina.com 
+    > Created Time: 2015年12月14日 星期一 11时57分05秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "hello world" << endl;
+}
